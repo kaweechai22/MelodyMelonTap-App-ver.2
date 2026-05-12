65be8e5bd1d8c7f33bf56f2e4ed480bb8d111a9c
kaweechai22 <cacarosjampa@gmail.com>
2026-05-12 22:02:36 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="melon-clean-stable-v19-2-naturalpause";
+const CACHE_NAME="melon-raw2-logic-v20";
 const ASSETS=["./","./index.html","./manifest.json","./icon.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))).then(()=>self.clients.claim()));});
